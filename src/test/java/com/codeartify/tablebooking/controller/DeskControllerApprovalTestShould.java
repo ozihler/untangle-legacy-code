11dd6832fb0a9c4e13b569105d5f8599d37f4e51
Oliver Zihler <github@codeartify.com>
2024-10-05 14:00:26 +0200

    step 3

--- src/test/java/com/codeartify/tablebooking/controller/DeskControllerApprovalTestShould.java
+++ src/test/java/com/codeartify/tablebooking/controller/DeskControllerApprovalTestShould.java
@@ -4,6 +4,7 @@ import com.codeartify.tablebooking.dto.ReservationRequest;
 import com.codeartify.tablebooking.model.Desk;
 import com.codeartify.tablebooking.repository.DeskRepository;
 import com.codeartify.tablebooking.repository.ReservationRepository;
+import com.codeartify.tablebooking.service.DeskReservationCheckerService;
 import com.codeartify.tablebooking.service.DeskService;
 import com.codeartify.tablebooking.service.ReservationService;
 import org.junit.jupiter.api.Test;
@@ -55,5 +56,31 @@ class DeskControllerApprovalTestShould {
         var response = deskController.reserveDesk(request);
 
         assertEquals(ResponseEntity.status(HttpStatus.CONFLICT).body("Desk is not available"), response);
+    }
+
+    @Test
+    void test3() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        when(deskRepository.findByAvailable(false)).thenReturn(List.of());
+        var desk = new Desk();
+        desk.setAvailable(true);
+        when(deskRepository.findById(1L)).thenReturn(Optional.of(desk));
+
+        DeskService deskService = new DeskService(deskRepository, null, null);
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        when(reservationRepository.findByReservedBy(any())).thenReturn(List.of());
+
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(null);
+        ReservationService reservationService = new ReservationService(null, reservationRepository, deskReservationCheckerService);
+        var deskController = new DeskController(null, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setDeskId(1L);
+        request.setRole("manager");
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while saving the reservation: Cannot invoke \"com.codeartify.tablebooking.repository.DeskRepository.save(Object)\" because \"this.deskRepository\" is null"), response);
     }
 }
