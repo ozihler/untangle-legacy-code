edea511553d2e6e95fbdee57ec7be58ebb2c4f0d
Oliver Zihler <github@codeartify.com>
2024-10-05 14:17:43 +0200

    step 9

--- src/test/java/com/codeartify/tablebooking/controller/DeskControllerApprovalTestShould.java
+++ src/test/java/com/codeartify/tablebooking/controller/DeskControllerApprovalTestShould.java
@@ -243,4 +243,41 @@ class DeskControllerApprovalTestShould {
 
         assertEquals(ResponseEntity.status(HttpStatus.CONFLICT).body("Requested desk is not adjustable."), response);
     }
+
+    @Test
+    void test9() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        when(deskRepository.findByAvailable(false)).thenReturn(List.of());
+        var desk = new Desk();
+        desk.setAvailable(true);
+        desk.setType("desk-type");
+        desk.setNearWindow(true);
+        desk.setHasMonitor(true);
+        desk.setAdjustable(true);
+        desk.setReservedForManager(true);
+
+        when(deskRepository.findById(1L)).thenReturn(Optional.of(desk));
+
+        DeskService deskService = new DeskService(deskRepository, null, null);
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        when(reservationRepository.findByReservedBy(any())).thenReturn(List.of());
+
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(null);
+        ReservationService reservationService = new ReservationService(null, reservationRepository, deskReservationCheckerService);
+        var deskController = new DeskController(null, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setDeskId(1L);
+        request.setRole("user");
+        request.setTeamMembers(new ArrayList<>());
+        request.setTypePreference("desk-type");
+        request.setNearWindow(true);
+        request.setNeedsMonitor(true);
+        request.setNeedsAdjustableDesk(true);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals(ResponseEntity.status(HttpStatus.FORBIDDEN).body("This desk is reserved for managers only."), response);
+    }
 }
