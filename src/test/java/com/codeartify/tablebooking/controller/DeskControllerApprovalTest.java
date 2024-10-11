4086041fb3e330a564a995c0c5e9d859ce48a2cf
Oliver Zihler <github@codeartify.com>
2024-10-11 14:19:26 +0200

    continue

--- src/test/java/com/codeartify/tablebooking/controller/DeskControllerApprovalTest.java
+++ src/test/java/com/codeartify/tablebooking/controller/DeskControllerApprovalTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -111,6 +112,262 @@ class DeskControllerApprovalTest {
         var response = deskController.reserveDesk(request);
 
         assertEquals("<409 CONFLICT Conflict,Desk is not available,[]>", response.toString());
+    }
+
+    @Test
+    void test6() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of(reservedBy));
+        request.setStartTime("08:00:00");
+        request.setEndTime("17:00:00");
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<409 CONFLICT Conflict,Team member team-member-name already has a reservation during the selected time.,[]>", response.toString());
+    }
+
+
+    @Test
+    void test7() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of(reservedBy));
+        request.setStartTime("08:00:00");
+        request.setEndTime("17:00:00");
+        request.setReservedBy(reservedBy);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<409 CONFLICT Conflict,There is already a reservation for the selected time.,[]>", response.toString());
+    }
+
+
+    @Test
+    void test8() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of());
+        request.setReservedBy(reservedBy);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<201 CREATED Created,Desk(id=2, available=false, location=null, hasMonitor=false, isAdjustable=false),[]>", response.toString());
+    }
+
+    @Test
+    void test9() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+        when(reservationRepository.save(any())).thenThrow(new RuntimeException());
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of());
+        request.setReservedBy(reservedBy);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<500 INTERNAL_SERVER_ERROR Internal Server Error,An error occurred while saving the reservation: null,[]>", response.toString());
+    }
+
+
+    @Test
+    void test10() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of());
+        request.setReservedBy(reservedBy);
+        request.setRecurring(true);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<400 BAD_REQUEST Bad Request,Recurring reservations must have a recurrence pattern.,[]>", response.toString());
+    }
+
+
+    @Test
+    void test11() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of());
+        request.setReservedBy(reservedBy);
+        request.setNeedsAdjustableDesk(true);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<409 CONFLICT Conflict,Requested desk is not adjustable.,[]>", response.toString());
+    }
+
+
+    @Test
+    void test12() {
+        DeskRepository deskRepository = mock(DeskRepository.class);
+        var reservedDesk = new Desk();
+        reservedDesk.setId(2L);
+        reservedDesk.setAvailable(true);
+        when(deskRepository.findByAvailable(true)).thenReturn(List.of(reservedDesk));
+
+        ReservationRepository reservationRepository = mock(ReservationRepository.class);
+        var teamMemberReservation = new Reservation();
+        var reservedBy = "team-member-name";
+        teamMemberReservation.setReservedBy(reservedBy);
+        teamMemberReservation.setDeskId(reservedDesk.getId());
+        teamMemberReservation.setStartTime("08:00:00");
+        teamMemberReservation.setEndTime("17:00:00");
+        when(reservationRepository.findByReservedBy(reservedBy)).thenReturn(List.of(teamMemberReservation));
+
+        RandomService randomService = new RandomService();
+        TeamDeskFinderService teamDeskFinderService = new TeamDeskFinderService(reservationRepository, randomService);
+        DeskService deskService = new DeskService(deskRepository, teamDeskFinderService, randomService);
+        DeskReservationCheckerService deskReservationCheckerService = new DeskReservationCheckerService(reservationRepository);
+        ReservationService reservationService = new ReservationService(deskRepository, reservationRepository, deskReservationCheckerService);
+
+        var deskController = new DeskController(deskRepository, null, reservationService, deskService);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setSitCloseToTeam(true);
+        request.setTeamMembers(List.of());
+        request.setReservedBy(reservedBy);
+        request.setNeedsMonitor(true);
+
+        var response = deskController.reserveDesk(request);
+
+        assertEquals("<409 CONFLICT Conflict,Requested desk does not have a monitor.,[]>", response.toString());
     }
 
 
